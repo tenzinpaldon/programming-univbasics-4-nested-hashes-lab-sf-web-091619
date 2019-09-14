241daec564aabf96b0c9ee1c55f328375909c0a4
Tenzin Paldon <tenzin.paldon16@gmail.com>
2019-09-14 19:56:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
         }
      }
 
+		 programmer_hash[:grace_hopper]
+
 
 end
 
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +59,14 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+ programmer_hash[:yukihiro_matsumoto] =  {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
 
 	programmer_hash = 
  		{
@@ -101,6 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
@@ -121,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
